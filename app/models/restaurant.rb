b8ffcdef9ff10caa9b5d867babd0d0ff444c2424
arielle nganso <arielle.nganso@gmail.com>
2022-11-16 16:53:43 +0100

    mvp rails

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
-  validates :name, :address, :category, presence: true
+  validates :name, :address, :category, :phone_number, presence: true
   validates :category, inclusion: { in: CATEGORY }
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+restaurant1 = Restaurant.new(name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '01 43 57 89 76', category: 'belgian')
+restaurant1.save
+restaurant2 = Restaurant.new(name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '01 43 57 16 35', category: 'japanese')
+restaurant2.save
+restaurant3 = Restaurant.new(name: 'La Perle de Dalian', address: '13 Rue Pétion, 75011 Paris', phone_number: '01 58 30 84 88', category: 'chinese')
+restaurant3.save
+restaurant4 = Restaurant.new(name: 'il bacaro', address: '9 Rue Auguste Laurent, 75011 Paris', phone_number: '01 48 07 45 48', category: 'italian')
+restaurant4.save
+restaurant5 = Restaurant.new(name: 'Les Papilles', address: '30 Rue Gay-Lussac, 75005 Paris', phone_number: '09 51 53 85 23', category: 'french')
+restaurant5.save
